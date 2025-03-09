31ced6be24897da54009728f0787cbd345406067
Vadim <matveev.vadim27@gmail.com>
2025-03-09 20:32:18 +0200

    bookStore

---
+++ src/store/bookStore.ts
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+
+export type Book = {
+  id?: number;
+  title: string;
+  author: string;
+  publisher: string;
+  genre: string;
+  image: string;
+  description: string;
+};
+
+export interface BooksStore {
+  books: Book[];
+  addBook: (book: Book) => void;
+  removeBook: (id: number) => void;
+}
+export const useBookStore = create<BooksStore>((set) => ({
+  books: [],
+  addBook: (book) =>
+    set((state) => ({ books: [...state.books, { id: Date.now(), ...book }] })),
+  removeBook: (id) =>
+    set((state) => ({ books: state.books.filter((book) => book.id !== id) })),
+}));
