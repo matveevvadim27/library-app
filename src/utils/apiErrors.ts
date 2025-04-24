ed23fc5dd5b54462cc348a4f1b34304d1e54534d
Vadim <matveev.vadim27@gmail.com>
2025-04-24 15:16:32 +0300

    Создание файла для работы с ошибками

---
+++ src/utils/apiErrors.ts
@@ -0,0 +1,32 @@
+import { toast } from "react-toastify";
+
+export const apiErrors = (error: any, customMessage?: string) => {
+  if (error.response) {
+    const { status } = error.response.status;
+
+    switch (status) {
+      case 400:
+        toast.warning(customMessage || "Неверый запрос");
+        break;
+      case 401:
+        toast.error(customMessage || "Вы не авторизованы");
+        break;
+      case 403:
+        toast.error(customMessage || "Доступ запрещён");
+        break;
+      case 404:
+        toast.info(customMessage || "Не найдено");
+        break;
+      case 500:
+        toast.error(customMessage || "Ошибка сервера");
+        break;
+      default:
+        toast.error(customMessage || "Произошла ошибка");
+        break;
+    }
+  } else if (error.request) {
+    toast.error(customMessage || "Нет ответа от сервера");
+  } else {
+    toast.error(customMessage || "Произошла ошибка");
+  }
+};
