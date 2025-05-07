975f340fb69967e9c3151108b91fa3cb17d7b1fd
Vadim <matveev.vadim27@gmail.com>
2025-05-07 12:00:57 +0300

    Исправлена проблема, когда не авторизованный пользователь мог видеть админские разделы

--- src/components/Shared/Bars/Navbar/Navbar.tsx
+++ src/components/Shared/Bars/Navbar/Navbar.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.scss";
+import { useAuthStore } from "store/useAuthStore";
 
 export default function Navbar() {
+  const { user } = useAuthStore();
+
   return (
     <nav className={styles.nav}>
       <ul className={styles.nav__list}>
@@ -15,16 +18,20 @@ export default function Navbar() {
             Книги
           </Link>
         </li>
-        <li className={styles.nav__item}>
-          <Link className={styles.nav__link} to="/librarian">
-            Библиотекарь
-          </Link>
-        </li>
-        <li className={styles.nav__item}>
-          <Link className={styles.nav__link} to="/admin">
-            Админ
-          </Link>
-        </li>
+        {user && user.role! <= 2 && (
+          <li className={styles.nav__item}>
+            <Link className={styles.nav__link} to="/librarian">
+              Библиотекарь
+            </Link>
+          </li>
+        )}
+        {user && user.role! === 1 && (
+          <li className={styles.nav__item}>
+            <Link className={styles.nav__link} to="/admin">
+              Админ
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
